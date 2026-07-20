1a17d5d64fe799f458fedde3a095ce90f0116f88
L-creator19 <lasyaa19@gmail.com>
2026-07-19 21:43:33 -0500

    corrected site name

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://l-creator19.github.io",
+  base: "/LassiA/",
   vite: {
     plugins: [tailwindcss()],
   },
